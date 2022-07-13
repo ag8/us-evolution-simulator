09cc5a4f601558d3d24636aa7db49e9acd431d49
Andrew G <andrew2000g@gmail.com>
2022-07-13 18:54:37 -0400

    I don't get why this isn't default

--- sim-60/ui.js
+++ sim-60/ui.js
@@ -9,7 +9,7 @@ let colorFunction = colorMap;
 function runNiceStep() {
 
     if (step === 0) {
-        text = "Welcome to Andrew's United States Simulator!"
+        text = "Welcome to Andrew's United States Simulator! Press the Step button above, or press space, to advance the simulation.";
     }
     if (step === 1) {
         text = "The idea is simple: what would happen if once a month, unhappy counties were allowed to secede, and join neighboring states?";
@@ -18,7 +18,7 @@ function runNiceStep() {
         text = "How do we decide if county wants to leave? The simplest heuristic is voting patterns. In this simulation, the unhappiness threshold is <b>60 points</b>: if a county is right next to a state that votes more than <i>60 points</i> closer to it than its current state, the county will secede, and join the state it agrees significantly more with.";
     }
     if (step === 3) {
-        text = "All right, let's get started. Here is the current United States of America.";
+        text = "All right, let's get started. Here are the current United States of America.";
         colorFunction();
     }
     if (step === 4) {
@@ -32,7 +32,7 @@ function runNiceStep() {
     }
     if (step === 7) {
         runStep();
-        text = "The county next to it, Allegany County, actually wants to join West Virginia more. However, the secession algorithm doesn't let a secession break a state apart, so Allegany needs to wait for Garrett County to secede first. In general, figuring out what the maximum \"unhappiness\" of counties that can secede is NP-complete, so we run a more realistic algorithm: the counties, in order of unhappiness, propose to leave the state; the state accepts all proposals that do not break the state into pieces.";
+        text = "The county next to it, Allegany County, actually wants to join West Virginia more. However, the secession algorithm doesn't let a secession break a state apart, so Allegany needs to wait for Garrett County to secede first. In general, figuring out what the maximum \"unhappiness\" of counties that can secede <a href=\"https://en.wikipedia.org/wiki/Steiner_tree_problem\" target='_blank'>is NP-complete</a>, so we run a more realistic algorithm: the counties, in order of unhappiness, propose to leave the state; the state accepts all proposals that do not break the state into pieces.";
     }
     if (step === 8) {
         text = "Anyways, let's hold another election.";
@@ -50,7 +50,7 @@ function runNiceStep() {
         text = "...".repeat(step - 10);
     }
     if (step === 15) {
-        text = "All, right, let's see what happens when the threshold isn't as unreasonably high&mdash;let's try, say, 50 points.";
+        text = "All, right, let's see what happens when the threshold isn't as unreasonably high&mdash;let's try, say, <b>50 points</b>.";
         threshold = 0.50;
     }
     if (step > 16 && step < 30) {
@@ -61,12 +61,12 @@ function runNiceStep() {
     }
     if (step === 31) {
         animateViewBox("20 40 400 200");
-        text = "For many years now, the <a href=\"https://www.greateridaho.org/\">Greater Idaho project</a> has been trying to move Oregon's border west, and have the eastern counties join Idaho. This simulation shows that these counties have a point---they vote more than 50 points closer to Idaho than to Oregon! In fact, every county that voted to join Idaho in real life did indeed join Idaho here; furthermore, Douglas and Josephine counties, which voted against joining Oregon in real life, did not join Idaho in the simulation either! Thus, 50% seems like a reasonable \"realistic\" threshold for whether counties <i>actually</i> want to leave their state, to the point of voting in favor of it.";
+        text = "For many years now, the <a href=\"https://www.greateridaho.org/\" target='_blank'>Greater Idaho project</a> has been trying to move Oregon's border west, and have the eastern counties join Idaho. This simulation shows that these counties have a point&mdash;they vote more than 50 points closer to Idaho than to Oregon! In fact, every county that voted to join Idaho in real life did indeed join Idaho here; furthermore, Douglas and Josephine counties, which voted against joining Oregon in real life, did not join Idaho in the simulation either! Thus, 50% seems like a reasonable \"realistic\" threshold for whether counties <i>actually</i> want to leave their state, to the point of voting in favor of it.";
     }
     if (step === 32) {
         animateViewBox("250 190 200 100");
         document.getElementById("c08123").style.fill = '#333';
-        text = "However, Weld County, CO, which in real life has voted to join Wyoming, has not yet joined Wyoming in the simulation. So, let's crank the threshold down to <b>40 points</b>, and see what happens.";
+        text = "However, Weld County, CO, which in real life <a href='https://www.weldcountywy.com/' target='_blank'>seriously wants to join Wyoming</a>, has not yet joined Wyoming in the simulation. So, let's crank the threshold down to <b>40 points</b>, and see what happens.";
     }
     if (step === 33) {
         animateViewBox("0 0 989.98 627.07");
@@ -94,23 +94,29 @@ function runNiceStep() {
         animateViewBox("0 0 989.98 627.07");
         text = "Anyways, enough talking; let's set the threshold to <b>20 points</b>! We've also added a button that automatically runs to equillibrium, if you're tired of pressing \"step\" each time.";
         document.getElementById("equil").style.display = 'inline-block';
+        document.getElementById("equil").disabled = false;
         threshold = 0.2;
     }
-    if (step > 68 && step < 81) {
+    if (step > 68 && step < 80) {
         runStep();
     }
-    if (step === 81) {
+    if (step === 80) {
         text = "Idaho is now a coastal state. Kansas barely exists. Pennsylvania now spans from Ohio to Vermont. Are you ready for a Wyoming-Texas border? Let's start the <b>10 point</b> threshold!";
         threshold = 0.1;
+        document.getElementById("equil").disabled = false;
     }
-    if (step > 81 && step < 111) {
+    if (step > 80 && step < 110) {
         runStep();
     }
-    if (step === 111) {
+    if (step === 110) {
         text = "New England still looks pretty recognizable. Everything else...isn't. Let's now set the threshold to <b>0</b>: if a county likes a neighboring state <i>any</i> more than its current state, it secedes and joins the neighbor. Seems pretty fair, right?";
         threshold = 0.0;
+        document.getElementById("equil").disabled = false;
     }
-    if (step > 111 && step < 131) {
+    if (step > 110 && step < 130) {
+        runStep();
+    }
+    if (step === 130) {
         text = "Ok, that's it! Every county that wanted to leave has left. Welcome to the new United States!";
     }
     if (step === 131) {
@@ -143,6 +149,8 @@ function runStep() {
     electionResults(changes);
     colorFunction();
     writeInfo();
+
+    return changes;
 }
 
 function electionResults(changes) {
@@ -158,15 +166,14 @@ function electionResults(changes) {
 // shape.setAttribute("viewBox", "630 30 400 200");
 
 function runNiceStepToEquillibrium() {
-    realStep++;
     step++;
-    let changed = timeStep();
-    colorFunction();
-    writeInfo();
-    updateText();
+
+    let changed = runStep();
 
     if (changed.length > 0) {
         setTimeout(runNiceStepToEquillibrium, 1000);
+    } else {
+        document.getElementById("equil").disabled = true;
     }
 }
 
